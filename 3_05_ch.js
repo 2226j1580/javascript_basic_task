fc7eed451b62c9379b9653715dc1e24d6c22e61e
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:21:03 +0530

    3_05_ch.js

---
+++ 3_05_ch.js
@@ -0,0 +1,22 @@
+
+        var book;
+
+
+book = {
+    title: "The Hobbit"
+};
+
+console.log("Book 1:");
+console.log(book);
+
+
+var book2 = {
+    title: "Harry Potter and the Philosopher's Stone"
+};
+
+
+console.log("Book 2:");
+console.log(book2);
+
+
+   
