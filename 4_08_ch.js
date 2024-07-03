b44cea754ab9f5ee2f7c199328022dfc8238dc58
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:08:28 +0530

    4_08_ch.js

---
+++ 4_08_ch.js
@@ -0,0 +1,24 @@
+
+
+var displayMenu = function () {
+    console.log("Please choose an option:");
+    console.log("(1) Print log");
+    console.log("(2) Upload file");
+    console.log("(3) Delete user");
+    console.log("(4) View statistics");
+    console.log("(9) Quit");
+};
+
+var displayMyMenu = function () {
+    console.log("Welcome to My Custom Menu:");
+    console.log("(1) View Profile");
+    console.log("(2) Edit Settings");
+    console.log("(3) View Notifications");
+    console.log("(4) Logout");
+};
+
+
+displayMenu();
+
+
+displayMyMenu();
