506d7d1ef2bf055d5aeb0fb7ab2e8ccbb8ec9519
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:13:55 +0530

    2_4_ch.js

---
+++ 2_4_ch.js
@@ -0,0 +1,14 @@
+
+     var score1 = 100;
+     console.log(score1);
+
+       score1 = 150;
+       console.log(score1);
+
+       var score2 = 200;
+     console.log(score2);
+
+       score2 = 600;
+       console.log(score2);
+        
+
