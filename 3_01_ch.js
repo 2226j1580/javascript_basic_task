067d6bd3b00d8dd0c0a70c735f3b53ccfc6f8e62
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:18:07 +0530

    3_01_ch.js

--- 3_01_ch.js
+++ 3_01_ch.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
+
         var bookTitle1, bookAuthor1;
         var bookTitle2, bookAuthor2;
         var bookTitle3, bookAuthor3;
@@ -62,6 +54,4 @@
         for (var i = 0; i < 10; i++) {
             console.log(bookTitles[i] + " by " + bookAuthors[i]);
         }
-        </script>
-</body>
-</html>
+  
