bb89605ee2c6593f19e61e21d6a79e647c5934f0
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:15:41 +0530

    2_7_ch.js

---
+++ 2_7_ch.js
@@ -0,0 +1,7 @@
+
+var playerName = "Kandra";
+var locationName = "The Dungeon of Doom";
+var health=70;
+
+console.log(playerName + " is in " + locationName);
+console.log(playerName +" has health "+health+" and is in "+locationName)
