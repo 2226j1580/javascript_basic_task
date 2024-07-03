339f3fb2cd542151db8f79336ea8e5e679dd60d0
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:35:03 +0530

    4_07_ch.js

--- 4_07_ch.js
+++ 4_07_ch.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
-        
 
 var number1 = 1000;
 var number2 = 66;
@@ -22,7 +12,3 @@ findTotal = function () {
 findTotal();
 
 console.log(number1 + " + " + number2 + " + " + number3 + " = " + result); 
-
-    </script>
-</body>
-</html>
