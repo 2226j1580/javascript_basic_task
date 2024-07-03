e42b0e21675aae9d9a44d651d7132c74cd9cf685
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:33:21 +0530

    4_05_ch.js

--- 4_05_ch.js
+++ 4_05_ch.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
+
         
 var findTotal;
 var displayMenu;
@@ -28,6 +20,4 @@ displayMenu = function () {
 findTotal();
 displayMenu();
 
-    </script>
-</body>
-</html>
+
