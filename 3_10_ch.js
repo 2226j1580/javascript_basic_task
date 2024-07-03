e09bc74fe1a4a9919ac7a04b2ae556ffe68d9714
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:25:35 +0530

    3_10_ch.js

--- 3_10_ch.js
+++ 3_10_ch.js
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
+
         var player1;
 
 player1 = {
@@ -26,6 +18,4 @@ player1.correct = 2;
 player1.score = 150;
 
 console.log(player1);
-    </script>
-</body>
-</html>
+  
