62917a99212646e3d2112ffe17b67f518d452085
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-03 22:15:06 +0530

    2_6_ch.js

---
+++ 2_6_ch.js
@@ -0,0 +1,14 @@
+
+var playerName;
+var locationName;
+var score;
+
+playerName = "Lionel Messi";
+locationName = "Argentina";
+score=300
+
+console.log(playerName + " is in " + locationName);
+console.log(playerName +" has a score of "+ score);
+
+
+
