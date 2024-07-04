fc3670797b5580e1b6f89034d06e32367b0a1b55
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-04 12:50:38 +0530

    5_03_ch.js

---
+++ 5_03_ch.js
@@ -0,0 +1,9 @@
+var showMessage;
+
+showMessage = function (message) {
+	console.log("The message is: " + message + " - That's amazing!"); 
+};
+
+showMessage("It's full of stars!");
+showMessage("Hello, world!"); 
+showMessage("This is another message."); 
