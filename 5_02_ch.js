4001fb1da6ecb288ae9e9e21a4db62b18d309fdd
2226j1580 <144468123+2226j1580@users.noreply.github.com>
2024-07-04 12:48:29 +0530

    5_02_ch.js

---
+++ 5_02_ch.js
@@ -0,0 +1,13 @@
+var msg;
+var msg2;
+
+msg = "It's full of stars!";
+msg2 = "without moon";
+
+showMessage = function () {
+	console.log(msg);
+	console.log(msg2); 
+  console.log(msg + ' ' + msg2);
+};
+
+showMessage();
